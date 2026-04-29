068861ea968933cd2777248d5fc07e8a535093c2
A5 <apisit@gmx.de>
2026-04-29 18:55:02 +0200

    Blatt5 1.a)

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,7 @@ class Main {
     public static void main(String[] args) {
         Fraction half = new Fraction (-12, -18);
         //Aufgabe 1
-        System.out.println(half.getNumerator());
-        System.out.println(half.getDenominator());
-
-        Medal goldMedal = new Medal("Donald", "Gold", 2026);
-        Medal goldMedalCapital = new Medal("Donald", "GOLD", 2026);
-        Medal silverMedal = new Medal("Mickey", "Silver",2026);
-
-        // Aufgabe  3
-        System.out.println(goldMedal.isSame(goldMedalCapital));
-        System.out.println(goldMedal.isSame(silverMedal));
-        System.out.println(silverMedal.isSame(silverMedal));
-
-        // Aufgabe 4
-        Medal m1 = new Medal("A", "b", 2020);
-        Medal m2 = new Medal("a", "b", 2020);
-        System.out.println(m1.isLess(m2));
-
+        // a)
+        System.out.println("Aufgabenblatt 5");
     }
 }
